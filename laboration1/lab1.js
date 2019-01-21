a9a3e1586415393fc0f46d43b44c7fba284c8039
Joakim Brandt <brjo1621@student.ju.se>
2019-01-21 19:12:53 +0100

    started lab 2

--- laboration1/lab1.js
+++ laboration1/lab1.js
@@ -1,22 +1,20 @@
-var headerList = []
+document.addEventListener("DOMContentLoaded", function() {
 
-		document.addEventListener("DOMContentLoaded", function() {
+setupTableOfContents()
 
-			setupTableOfContents()
+})
 
-		})
+function setupTableOfContents() {
 
-		function setupTableOfContents() {
+	const newList = document.createElement("ul")
+	const headers = document.querySelectorAll("h2")
 
-			const newList = document.createElement("ul")
-			const headers = document.querySelectorAll("h2")
+	for (let index = 0; index < headers.length; index++) {
+		var listItem = document.createElement("li")
+		listItem.innerText = headers[index].innerText
+		newList.appendChild(listItem)
+	}
 
-			for (let index = 0; index < headers.length; index++) {
-				var listItem = document.createElement("li")
-				listItem.innerText = headers[index].innerText
-				newList.appendChild(listItem)
-			}
+	document.getElementById("tableOfContents").append(newList)
 
-			document.getElementById("tableOfContents").append(newList)
-			
-		}
+}
